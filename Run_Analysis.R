9826e5af2b4bfedb3839efc30ea88f191192f7b5
acallebaut <acallebaut@deloitte.com>
2017-01-15 18:48:46 +0100

    Completed Code

--- Run_Analysis.R
+++ Run_Analysis.R
@@ -1,3 +1,70 @@
 library(data.table)
 library(dplyr)
 
+# Read datasets
+
+getwd()
+# TEST DATASET
+subject_test <- read.table(paste0(getwd(),"/UCIHARDataset/test/subject_test.txt"))
+X_test <- read.table(paste0(getwd(),"/UCIHARDataset/test/X_test.txt"))                           
+Y_test <- read.table(paste0(getwd(),"/UCIHARDataset/test/Y_test.txt"))
+
+# TRAIN DATASET
+subject_train <- read.table(paste0(getwd(),"/UCIHARDataset/train/subject_train.txt"))
+X_train <- read.table(paste0(getwd(),"/UCIHARDataset/train/X_train.txt"))                           
+Y_train <- read.table(paste0(getwd(),"/UCIHARDataset/train/Y_train.txt"))
+
+features_Names <- read.table(paste0(getwd(),"/UCIHARDataset/features.txt"), col.names=c("feature_ID", "feature_Name"))
+activity_labels <- read.table(paste0(getwd(), "/UCIHARDataset/activity_labels.txt"), col.names=c("activity_ID", "activity_Name"))
+
+# Merge the datasets by subject, feature and activity
+subject <- rbind(subject_test, subject_train)
+features <- rbind(X_test, X_train)
+activity <- rbind(Y_test, Y_train)
+
+# Give name to columns. 
+colnames(subject) <- "Subject"
+colnames(features) <- t(features_Names$feature_Name)
+colnames(activity) <- "Activity"
+
+# Merge subject, features and activity. 
+cleandata <- cbind(subject, features, activity)
+
+# Create a new dataframe by extracting columns where columnname contains "mean" or "Std". 
+mydata <- grep(".*Mean.*|.*Std.*", names(cleandata), ignore.case=TRUE)
+meanSD_col <- c(mydata, 1, 563)
+cleandata_meanSD <- cleandata[,meanSD_col]
+
+# Use descriptive activity names to name the activities in the data set
+# Replace the old name by the new name. 
+# Factor the activities
+cleandata_meanSD$Activity <- as.character(cleandata_meanSD$Activity)
+for (i in 1:6){
+  cleandata_meanSD$Activity[cleandata_meanSD$Activity == i] <- as.character(activity_labels[i,2])
+}
+cleandata_meanSD$Activity <- as.factor(cleandata_meanSD$Activity)
+
+# Appropriately labels the data set with descriptive variable names. 
+
+names(cleandata_meanSD)<-gsub("Acc", "Accelerometer", names(cleandata_meanSD))
+names(cleandata_meanSD)<-gsub("Gyro", "Gyroscope", names(cleandata_meanSD))
+names(cleandata_meanSD)<-gsub("BodyBody", "Body", names(cleandata_meanSD))
+names(cleandata_meanSD)<-gsub("Mag", "Magnitude", names(cleandata_meanSD))
+names(cleandata_meanSD)<-gsub("^t", "Time", names(cleandata_meanSD))
+names(cleandata_meanSD)<-gsub("^f", "Frequency", names(cleandata_meanSD))
+names(cleandata_meanSD)<-gsub("tBody", "TimeBody", names(cleandata_meanSD))
+names(cleandata_meanSD)<-gsub("-mean()", "Mean", names(cleandata_meanSD), ignore.case = TRUE)
+names(cleandata_meanSD)<-gsub("-std()", "STD", names(cleandata_meanSD), ignore.case = TRUE)
+names(cleandata_meanSD)<-gsub("-freq()", "Frequency", names(cleandata_meanSD), ignore.case = TRUE)
+names(cleandata_meanSD)<-gsub("angle", "Angle", names(cleandata_meanSD))
+names(cleandata_meanSD)<-gsub("gravity", "Gravity", names(cleandata_meanSD))
+
+# From the data set in step 4, creates a second, 
+# independent tidy data set with the average of each variable 
+# for each activity and each subject.
+
+cleandata_meanSD <- data.table(cleandata_meanSD)
+tidyData <- aggregate(. ~Subject + Activity, cleandata_meanSD, mean)
+tidyData <- tidyData[order(tidyData$Subject,tidyData$Activity),]
+write.table(tidyData, file = "TidyData.txt", row.names = FALSE)
+
